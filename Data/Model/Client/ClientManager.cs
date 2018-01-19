7f015db6a91065e0bf5074624937aa31d6b1f3c8
CB006597 CB006597 <CB006597@students.apiit.lk>
2018-01-19 08:37:22 +0530

    Data And Domain Layer Complete

---
+++ Data/Model/Client/ClientManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Repository;
+
+namespace Data.Model.Client
+{
+    public class ClientManager : IServerRequest
+    {
+        private UserEntity _userEntity;
+        private IMessageReceiver _messageReceiver;
+        public ClientManager(UserEntity userEntity, IMessageReceiver messageReceiver)
+        {
+            _userEntity = userEntity;
+            _messageReceiver = messageReceiver;
+            _userEntity.SocketManager.Received += Client_Received;
+        }
+
+        private void Client_Received(string message)
+        {
+            _messageReceiver.Received(message);
+        }
+
+        public void Send(string message)
+        {
+            _userEntity.SocketManager.Send(message);
+        }
+
+        public void Disconnect()
+        {
+            _userEntity.SocketManager.Close();
+        }
+
+        public void Connect(string ipAddress)
+        {
+            _userEntity.SocketManager.Connect(ipAddress, 2014);
+        }
+    }
+}
---
+++ Data/Model/Client/UserEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Model;
+
+namespace Data.Model.Client
+{
+    public class UserEntity
+    {
+        private User _user;
+        private SocketManager _socketManager;
+
+        public User User
+        {
+            get => _user;
+            set => _user = value;
+        }
+
+        public SocketManager SocketManager
+        {
+            get => _socketManager;
+            set => _socketManager = value;
+        }
+
+        public UserEntity(User user, SocketManager socketManager)
+        {
+            _user = user;
+            _socketManager = socketManager;
+        }
+
+
+    }
+}
